d8421fcb2f2292e7b80b060b1a00bfd6fcebdd9d
teieigun <teieigun@gmail.com>
2020-08-15 21:12:13 +0900

    华容道式的布局

--- app/src/main/java/com/example/teiapp/MainActivity.java
+++ app/src/main/java/com/example/teiapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.teiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //找到按钮
+        mBtnButton =(Button)findViewById(R.id.button01);
+        mBtnButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
