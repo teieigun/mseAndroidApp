283c7f9c5106ce509a2f78ddbf286dc594696d61
teieigun <teieigun@gmail.com>
2020-08-16 11:43:29 +0900

    华容道式的布局

--- app/src/main/java/com/example/teiapp/MainActivity.java
+++ app/src/main/java/com/example/teiapp/MainActivity.java
@@ -12,17 +12,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Button mBtnButton;
+    private Button mBtnButtonForEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //--------------------------------------------------------------------------------------------//
         //找到按钮
         mBtnButton =(Button)findViewById(R.id.button03);
-        //增加此按钮的监听
+        //增加此按钮的监听--用此按钮测试TextView
         mBtnButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //由 Intent来协助完成 Android各个组件之间的通讯
                 Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //--------------------------------------------------------------------------------------------//
+        mBtnButtonForEditText =(Button)findViewById(R.id.button02);
+        //增加此按钮的监听--用此按钮测试EditText
+        mBtnButtonForEditText.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //由 Intent来协助完成 Android各个组件之间的通讯
+                Intent intent = new Intent(MainActivity.this, EditTextActivity.class);
                 startActivity(intent);
             }
         });
