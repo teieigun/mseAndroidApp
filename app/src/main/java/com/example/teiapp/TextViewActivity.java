840bb0d04cdd37f6aa62dd2c2c5ba9c3ce08bd4c
teieigun <teieigun@gmail.com>
2020-08-30 13:16:26 +0900

    学习线性布局

--- app/src/main/java/com/example/teiapp/TextViewActivity.java
+++ app/src/main/java/com/example/teiapp/TextViewActivity.java
@@ -9,6 +9,6 @@ public class TextViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_view);
+        setContentView(R.layout.activity_see_view);
     }
 }
